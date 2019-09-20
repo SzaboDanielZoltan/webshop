caabb6bece4c29a3631a6e70b1551242d5f7cfa0
neslanovic <timy1901@gmail.com>
2019-09-20 17:26:34 +0200

    fixing

--- webshop-express/routes/index.js
+++ webshop-express/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
   res.render('products');
 });
 
-// /* pls let run once and it will fill the postfix in SQL  */
+// /* pls let run once and it will fill the postfix in   */
 // router.get('/', async (req, res, next) => {
 //   const all = await db.getProducts();
 //   for (let i = 0; i < all.length + 1; i += 1) {
