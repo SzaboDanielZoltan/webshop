f8a35420568ae7ae29e101eef9a0103217e210b7
KisSzenty <kisszenty@gmail.com>
2019-09-21 14:17:26 +0200

    feat: product-list-new-edit components & product service added

--- webshop-angular/src/app/app.module.ts
+++ webshop-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './page/product-list/product-list.component';
+import { ProductNewComponent } from './page/product-new/product-new.component';
+import { ProductEditComponent } from './page/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductNewComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
