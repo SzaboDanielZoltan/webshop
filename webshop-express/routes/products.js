9b19ce72da8240a7a1c8ed2c2bfa7350f4689a93
neslanovic <timy1901@gmail.com>
2019-09-22 09:17:04 +0200

    Fit:add  more criterium for the pagination

--- webshop-express/routes/products.js
+++ webshop-express/routes/products.js
@@ -4,27 +4,46 @@ const db = new bll();
 
 const router = express.Router();
 
-/* GET products page in order by productName ASC */
+/* GET products page in order by productName ASC + pagination logic */
 router.get('/', async (req, res, next) => {
     const result = await db.getProductsInOrder()
     const resultSize = result.length
-    
+
     /*If you have query string, then this will run, if not just normal render*/
-    if(req.query.limit && req.query.page != undefined){
+    if (req.query.limit && req.query.page != undefined) {
         let getData = [];
-        result.forEach((data, index) => { 
-        if (index < (req.query.page*req.query.limit)){
-            getData.push(data)
+        result.forEach((data, index) => {
+            if (index < (req.query.page * req.query.limit)) {
+                getData.push(data)
+            }
+        });
+        if (req.query.page <= 1) {
+            let previosPage = 1;
+            let nextOnePage = parseInt(req.query.page) + 1;
+            let currentPageData = getData.slice(getData.length - req.query.limit, getData.length)
+            return res.render('products', {
+                products: currentPageData, numberOfproducts: resultSize,
+                prevPage: previosPage, nextPage: nextOnePage
+            })
+        } else if (req.query.page >= resultSize / req.query.limit) {
+            let previosPage = 1;
+            let nextOnePage = Math.floor(resultSize / req.query.limit)
+            let currentPageData = getData.slice(getData.length - req.query.limit, getData.length)
+            return res.render('products', {
+                products: currentPageData, numberOfproducts: resultSize,
+                prevPage: previosPage, nextPage: nextOnePage
+            })
         }
-    });
-    let previosPage = req.query.page - 1;
-    let nextOnePage = parseInt(req.query.page)+1;
-    let currentPageData = getData.slice(getData.length - req.query.limit,getData.length)
-    return res.render('products', { products: currentPageData, numberOfproducts : resultSize,
-    prevPage: previosPage , nextPage : nextOnePage})
+        let previosPage = req.query.page - 1;
+        let nextOnePage = parseInt(req.query.page) + 1;
+        let currentPageData = getData.slice(getData.length - req.query.limit, getData.length)
+        return res.render('products', {
+            products: currentPageData, numberOfproducts: resultSize,
+            prevPage: previosPage, nextPage: nextOnePage
+        })
     }
 
-    res.render('products', { products: result , numberOfproducts : resultSize});
+    res.render('products', { products: result, numberOfproducts: resultSize });
 });
 
 /* GET product detail page */
