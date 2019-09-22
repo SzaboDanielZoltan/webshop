a19e330301a57397069ae194c906f3b965d66624
KisSzenty <kisszenty@gmail.com>
2019-09-22 19:47:33 +0200

    feat: product-list & new template upgrade

--- webshop-angular/src/app/page/product-new/product-new.component.ts
+++ webshop-angular/src/app/page/product-new/product-new.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-product-new',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductNewComponent implements OnInit {
 
-  constructor() { }
+  newProduct: Product = new Product();
+
+  constructor(private ps: ProductService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onCreate() {
+    this.ps.create(this.newProduct).forEach(
+      data => this.router.navigateByUrl('/')
+    )
   }
 
 }
