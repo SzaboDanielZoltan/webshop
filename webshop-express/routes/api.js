3651cf807be65bd71ffd586178f222dbbb4fe4d3
Napsugar Mako <jnmako@gmail.com>
2019-09-23 16:45:28 +0200

    Feat: /api/customers methods added

--- webshop-express/routes/api.js
+++ webshop-express/routes/api.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bll = require('./../bll/products');
 const db = new bll();
 const router = express.Router();
+const bllCustomer = require('./../bll/customers');
+const dbCustomer = new bllCustomer();
 
+/* Product admin methods */
 router.get('/', async (req, res, next) => {
   res.json({ message: 'Server works!' });
 });
@@ -23,5 +26,22 @@ router.put('/products/:id', async (req, res, next) => {
   const result = await db.updateProduct(req.body);  /* , req.params.id*/
   res.json(result);
 })
+
+/* Customer admin methods */
+router.get('/customers', async (req, res, next) => {
+  const result = await dbCustomer.getCustomers();
+  res.json(result);
+});
+
+router.delete('/customers/:id', async (req, res, next) => {
+  const result = await dbCustomer.deleteCustomer(req.params.id);
+  res.json(result);
+})
+
+router.put('/customers/:id', async (req, res, next) => {
+  const result = await dbCustomer.updateCustomer(req.body);  /* , req.params.id*/
+  res.json(result);
+})
+
 
 module.exports = router;
