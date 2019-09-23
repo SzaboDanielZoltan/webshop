d54ad59276e669612b8ee92ce070b39ce3d2d6c8
Szabo Daniel Zoltan <szabo.daniel.zoltan@gmail.com>
2019-09-23 15:52:35 +0200

    Feat: admin token validation, redireict from /admin route if not valid

---
+++ webshop-express/routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const AdminsBLL = require('./../bll/admins');
+
+const adminsBLL = new AdminsBLL();
+const router = express.Router();
+
+router.get('/admin', async (req, res, next) => {
+  const validAdmin = await adminsBLL.adminTokenValidator(req.cookies.adminvalidator);
+  if (validAdmin) {
+    res.render('admin');
+  }
+  res.redirect('/');
+});
+
+router.get('/admin/*', async (req, res, next) => {
+  const validAdmin = await adminsBLL.adminTokenValidator(req.cookies.adminvalidator);
+  if (validAdmin) {
+    res.redirect('/admin');
+  }
+  res.redirect('/');
+});
+
+module.exports = router;
