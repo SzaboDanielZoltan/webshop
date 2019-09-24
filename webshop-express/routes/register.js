468e84e7549aff135aa2b9a6ccc169f8eafea190
KisSzenty <kisszenty@gmail.com>
2019-09-24 17:59:21 +0200

    feat: registration successful message added

--- webshop-express/routes/register.js
+++ webshop-express/routes/register.js
@@ -6,18 +6,20 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    res.render('register');
-  });
+  res.render('register');
+});
 
- router.post('/', async (req, res, next)=>{
-   console.log(req.body.email);
-    let checkEmail = await customersBLL.validateEmail(req.body.email)  
-    if (checkEmail == true ){
-      let result = await customersBLL.createCustomer(req.body);
-      return res.redirect('/')
-    }
-    const message = "This customer is already exist"
-    res.render('register', {notValidmessage: message})  
- }) 
-  
+router.post('/', async (req, res, next) => {
+  console.log(req.body.email);
+  let checkEmail = await customersBLL.validateEmail(req.body.email)
+  if (checkEmail == true) {
+    let result = await customersBLL.createCustomer(req.body);
+    const successMessage = "The registraition is successful. Now You can logging in!"
+
+    return res.render('login', { success: successMessage })
+  }
+  const message = "This customer is already exist"
+  res.render('register', { notValidmessage: message })
+})
+
 module.exports = router;
