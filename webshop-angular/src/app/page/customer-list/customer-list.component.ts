3793e5d1126b0985d891bfad9f87ba244aa761e8
Napsugar Mako <jnmako@gmail.com>
2019-09-24 10:52:10 +0200

    Feat: Customer admin page works, searching by email, edit & delete functions fixed

--- webshop-angular/src/app/page/customer-list/customer-list.component.ts
+++ webshop-angular/src/app/page/customer-list/customer-list.component.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CustomerService } from 'src/app/service/customer.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { ConstantPool } from '@angular/compiler';
+import { Customer } from 'src/app/model/customer';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
   styleUrls: ['./customer-list.component.css']
 })
-export class CustomerListComponent implements OnInit {
-
-  customerList$: Observable<any> = this.cs.read();
-  // searchCustomer: string = '';
-  // changeCounter: number = 0;
+export class CustomerListComponent implements OnInit, OnDestroy {
+  customerSubscription: Subscription;
+  customerList$: Array<Customer>;
+  searchCustomer: string = '';
+  changeCounter: number = 0;
 
   constructor(private cs: CustomerService) {
-    cs.access();
   }
 
   ngOnInit() {
+this.customerSubscription = this.cs.read().subscribe(
+  customers => {
+    this.customerList$ = customers;
+  },
+  err => console.error(err)
+);
   }
 
+ngOnDestroy() {
+  this.customerSubscription.unsubscribe();
+}
+
   onCustomerDelete(id: number): void {
-    this.cs.delete(id).forEach(data => location.reload());
+    this.cs.delete(id).forEach(data => {
+      let index = this.customerList$.findIndex(customer => customer.id == id);
+      this.customerList$.splice(index, 1);
+      this.changeCounter++;
+    });
   }
 }
--- webshop-angular/src/app/pipe/customer-filter.pipe.ts
+++ webshop-angular/src/app/pipe/customer-filter.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CustomerFilterPipe implements PipeTransform {
 
   transform(value: any, searchCustomer: string, changeCounter) {
-    return value.filter(item => item.userName.toLowerCase().indexOf(searchCustomer.toLowerCase()) > -1);
+    return value.filter(item => item.email.toLowerCase().indexOf(searchCustomer.toLowerCase()) > -1);
   }
 
 }
