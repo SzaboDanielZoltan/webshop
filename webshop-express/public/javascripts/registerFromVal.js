6486eb9875da7d3a73d3ee15f7cff701e67ec0c6
neslanovic <timy1901@gmail.com>
2019-09-25 15:20:57 +0200

    password strengthen without validation

--- webshop-express/public/javascripts/registerFromVal.js
+++ webshop-express/public/javascripts/registerFromVal.js
@@ -90,3 +90,33 @@ function formCheckValidation() {
     }
     return true
 }
+
+function validatePassword(){
+
+    var strength = {
+        0: "Worst",
+        1: "Bad",
+        2: "Weak",
+        3: "Good",
+        4: "Strong"
+      }
+      var password = document.getElementById('password');
+      var meter = document.getElementById('password-strength-meter');
+      var text = document.getElementById('password-strength-text');
+      
+      password.addEventListener('input', function() {
+        document.querySelector('meter').setAttribute('style', 'display : block')  
+        var val = password.value;
+        var result = zxcvbn(val);
+      
+        // Update the password strength meter
+        meter.value = result.score;
+      
+        // Update the text indicator
+        if (val !== "") {
+          text.innerHTML = "Strength: " + strength[result.score]; 
+        } else {
+          text.innerHTML = "";
+        }
+      });
+}
