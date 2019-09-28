0a61bc74aaa8463b9b831c5cbcf883e469ee10ef
neslanovic <timy1901@gmail.com>
2019-09-28 08:32:51 +0200

    navbar scroll event

--- webshop-express/public/javascripts/design.js
+++ webshop-express/public/javascripts/design.js
@@ -1,3 +1,13 @@
+window.onscroll = () => {
+  const nav = document.querySelector('nav');
+  const top = document.body.scrollTop || document.documentElement.scrollTop;
+  if (top !== 0) {
+    nav.classList.add('scrolled-nav');
+  } else {
+    nav.classList.remove('scrolled-nav');
+  }
+};
+
 $(function () {
     $('[data-toggle="popover"]').popover()
   })
