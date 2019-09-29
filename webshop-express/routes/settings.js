9672a3b056524f369fb46a82bbf9ee66cf20910b
Szabo Daniel Zoltan <szabo.daniel.zoltan@gmail.com>
2019-09-29 21:09:33 +0200

    Feat: user settings base ready

--- webshop-express/routes/settings.js
+++ webshop-express/routes/settings.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sha1 = require('js-sha1');
 const CustomerBLL = require('../bll/customers');
 
 const customerBLL = new CustomerBLL();
@@ -8,6 +9,29 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('customerSettings');
+});
+
+router.post('/:attribute', async (req, res, next) => {
+  if (req.params.attribute === 'password') {
+    if (sha1(req.body.actualPassword) === res.locals.loggedcustomer.password
+      && req.body.newPassword1 === req.body.newPassword2) {
+      res.locals.loggedcustomer.password = sha1(req.body.newPassword1);
+      await customerBLL.updateCustomer(res.locals.loggedcustomer);
+      res.render('customerSettings', { message: `You changed your ${req.params.attribute.toLowerCase()} successfully` });
+    } else if (sha1(req.body.actualPassword) !== res.locals.loggedcustomer.password) {
+      res.render('customerSettings', { alert: `Your actual ${req.params.attribute.toLowerCase()} is wrong!` });
+    } else {
+      res.render('customerSettings', { alert: `New ${req.params.attribute.toLowerCase()}s are not matching!` });
+    }
+  } else {
+    res.locals.loggedcustomer[req.params.attribute] = req.body[req.params.attribute];
+    await customerBLL.updateCustomer(res.locals.loggedcustomer);
+    res.render('customerSettings', { message: `You changed your ${req.params.attribute.toLowerCase()} successfully` });
+  }
+});
+
+router.get('/:attribute', async (req, res, next) => {
+  res.render('customerSettings', { attr: req.params.attribute });
 });
 
 module.exports = router;
