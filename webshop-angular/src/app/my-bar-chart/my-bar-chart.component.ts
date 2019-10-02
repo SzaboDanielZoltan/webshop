9372d83c87f6c1d97f5ec6bfe666be0af1e277b2
Horvath Szilvia <horvath.szilvia.ildiko@gmail.com>
2019-10-02 15:27:35 +0200

    charts

--- webshop-angular/src/app/my-bar-chart/my-bar-chart.component.ts
+++ webshop-angular/src/app/my-bar-chart/my-bar-chart.component.ts
@@ -6,18 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-bar-chart.component.css']
 })
 export class MyBarChartComponent implements OnInit {
-  constructor() { }
+
+  showChart: boolean = false;
+
+  constructor() {
+  }
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
   };
-  public barChartLabels = ['2015', '2016', '2017', '2016', '2017', '2018', '2019'];
+  public barChartLabels = ['2015', '2016', '2017', '2016', '2017', '2018', '2019']
   public barChartType = 'bar';
   public barChartLegend = true;
-  public barChartData = [
-    {data: [65, 59, 70, 81, 56, 70, 40], label: 'Income form Human'},
-    {data: [28, 48, 40, 19, 86, 60, 87], label: 'Income from Alien'}
-  ];
+  public barChartData = [];
+
   ngOnInit() {
+    setTimeout(()=>{
+
+      this.barChartData = [
+        {data: [80, 59, 70, 81, 56, 70, 40], label: 'Income from Human'},
+        {data: [28, 48, 40, 19, 86, 60, 87], label: 'Income from Alien'}
+      ];
+
+
+      this.showChart = true;
+    },3000)
   }
 }
